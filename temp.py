fd592d8506f133eaba877bc0e3229cf6084baeba
Ami Raj <ami.raj2307@gmail.com>
2025-09-09 19:37:20 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,6 @@
-temp_fahrenheit_input = input("Enter temperature in Fahrenheit: ")
-temp_fahrenheit = float(temp_fahrenheit_input)
-def fahrenheit_to_celsius(fahrenheit: float) -> float:
-    celsius = (5 / 9) * (fahrenheit - 32)
-    return celsius
-
-temp_celsius = fahrenheit_to_celsius(temp_fahrenheit)
-print(f"Temperature in Celsius: {temp_celsius}")
-
+ temp_fahrenheit = float(input("Enter temperature in Fahrenheit:-"))
+def in_celsius(F):
+    C=(5/9)*(F - 32)
+    TEMP = C
+    return TEMP
+print("Temperature in Celsius:",in_celsius(temp_fahrenheit))
