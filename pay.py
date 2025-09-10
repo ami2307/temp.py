bba275b853d35208034a142cf2f5381c0425fcf5
Ami Raj <ami.raj2307@gmail.com>
2025-09-10 19:28:38 -0400

    Update pay.py

--- pay.py
+++ pay.py
@@ -3,15 +3,15 @@ import math
 def unfair_weekly_paycheck_amount(hours_worked: float) -> int:
     return math.floor(hours_worked) * 15
 
-hours = float(input("Enter Hours for unfair pay:"))
-print(unfair_weekly_paycheck_amount(hours))
-
-
+if __name__ == "__main__":
+    hours = float(input())
+    print(unfair_weekly_paycheck_amount(hours))
 
 def fair_weekly_paycheck_amount(hours_worked: float) -> float:
     return hours_worked * 15.0
 
-hours = float(input("Enter Hours for fair pay:"))
-print(f"{fair_weekly_paycheck_amount(hours):.2f}")
+if __name__ == "__main__":
+    hours = float(input())
+    print(f"{fair_weekly_paycheck_amount(hours):.2f}")
 
 
