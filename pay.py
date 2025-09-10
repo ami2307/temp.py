12aa5506fcb435fdcd7ef62548c142d3f6de8643
Ami Raj <ami.raj2307@gmail.com>
2025-09-10 18:10:26 -0400

    Update pay.py

--- pay.py
+++ pay.py
@@ -6,12 +6,9 @@ def unfair_weekly_paycheck_amount(hours_worked: float) -> int:
 def fair_weekly_paycheck_amount(hours_worked: float) -> float:
     return hours_worked * 15.0
 
-print("Unfair Pay:")
-print(unfair_weekly_paycheck_amount(2))       
-print(unfair_weekly_paycheck_amount(2.5))     
-print(unfair_weekly_paycheck_amount(100.75))  
-
-print("\nFair Pay:")
-print(fair_weekly_paycheck_amount(2))         
-print(fair_weekly_paycheck_amount(2.5))       
-print(fair_weekly_paycheck_amount(100.75))   
+print(unfair_weekly_paycheck_amount(2))
+print(unfair_weekly_paycheck_amount(2.5))
+print(unfair_weekly_paycheck_amount(100.75))
+print(fair_weekly_paycheck_amount(2))
+print(fair_weekly_paycheck_amount(2.5))
+print(fair_weekly_paycheck_amount(100.75))
